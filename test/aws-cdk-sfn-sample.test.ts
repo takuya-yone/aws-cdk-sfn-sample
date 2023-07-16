2c13f1814445ccbe07bf023676afc36c35b5de78
Takuya Yonezawa <yonezawa.takuya.yq2@gmail.com>
2023-07-16 19:37:09 +0900

    up

---
+++ test/aws-cdk-sfn-sample.test.ts
@@ -0,0 +1,16 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkSfnSample from '../lib/aws-cdk-sfn-sample-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-sfn-sample-stack.ts
+test('SQS Queue Created', () => {
+  //   const app = new cdk.App();
+  //     // WHEN
+  //   const stack = new AwsCdkSfnSample.AwsCdkSfnSampleStack(app, 'MyTestStack');
+  //     // THEN
+  //   const template = Template.fromStack(stack);
+  //   template.hasResourceProperties('AWS::SQS::Queue', {
+  //     VisibilityTimeout: 300
+  //   });
+});
