c67ce2a30ebb93877d44abd9262a30268e33f05c
Takuya Yonezawa <yonezawa.takuya.yq2@gmail.com>
2023-07-16 19:51:29 +0900

    up

--- test/aws-cdk-sfn-sample.test.ts
+++ test/aws-cdk-sfn-sample.test.ts
@@ -1,16 +1,13 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as AwsCdkSfnSample from '../lib/aws-cdk-sfn-sample-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import { AwsCdkSfnSampleStack } from '../lib/aws-cdk-sfn-sample-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/aws-cdk-sfn-sample-stack.ts
-test('SQS Queue Created', () => {
-  //   const app = new cdk.App();
-  //     // WHEN
-  //   const stack = new AwsCdkSfnSample.AwsCdkSfnSampleStack(app, 'MyTestStack');
-  //     // THEN
-  //   const template = Template.fromStack(stack);
-  //   template.hasResourceProperties('AWS::SQS::Queue', {
-  //     VisibilityTimeout: 300
-  //   });
+test('Unit Test', () => {
+  const app = new cdk.App();
+  const stack = new AwsCdkSfnSampleStack(app, 'AwsCdkSfnSampleStack', {});
+  // Snapshot Test
+  expect(Template.fromStack(stack)).toMatchSnapshot();
+
+  // Fine-grained assertions
+  const template = Template.fromStack(stack);
 });
